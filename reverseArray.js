5a5d09f64d3d43afff2519e667aebe2d8e78560c
TasneemALMAHROUQ <146703381+TasneemALMAHROUQ@users.noreply.github.com>
2025-05-12 10:21:22 +0300

    Update reverseArray.js

--- reverseArray.js
+++ reverseArray.js
@@ -1,13 +1,12 @@
 function reverseArray(arr) {
-  const reversedArray = new Array(arr.length); // مصفوفة جديدة
+  const reversedArray = new Array(arr.length); 
 
   for (let i = 0; i < arr.length; i++) {
-    reversedArray[arr.length - 1 - i] = arr[i]; // نعكس العناصر
-  }
+    reversedArray[arr.length - 1 - i] = arr[i];
 
   return reversedArray;
 }
 
-// تجربة الدالة
+
 const result = reverseArray([10, 20, 30, 40, 50]);
 console.log("Reversed Array:", result);
